15b773c955eec0a6476d3721c1d548f075603179
tsand12 <tiaerrasanders2012@gmail.com>
2019-02-11 13:06:09 -0600

    Missed some files

---
+++ src/main/java/Controller/Controller.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import org.thymeleaf.ITemplateEngine;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Controller {
+
+    public void process(
+            HttpServletRequest request, HttpServletResponse response,
+            ServletContext servletContext, ITemplateEngine templateEngine)throws Exception;
+}
---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@org.springframework.stereotype.Controller
+public class HomeController implements Controller {
+
+    public HomeController(){
+        super();
+    }
+    /*@RequestMapping(value="/home", method= RequestMethod.GET)
+    String home(){
+        return "home";
+    }*/
+
+
+    public void process(
+            final HttpServletRequest request, final HttpServletResponse response, final ServletContext servletContext,
+            final ITemplateEngine templateEngine)throws Exception{
+
+        WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+
+        templateEngine.process("home", ctx, response.getWriter());
+    }
+}
