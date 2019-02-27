554ba99f7a7ac44bcf24d7149123b2b84f4ba86e
tsand12 <tiaerrasanders2012@gmail.com>
2019-02-27 12:41:23 -0600

    Disabled hibernate to correctly save user info. Flyway is now correctly set up and users are being saved to h2 db. Also added some minor bootstrap styles on results page.

---
+++ src/main/java/Practice1/User.java
@@ -0,0 +1,77 @@
+package practice1;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+   // private String authToken;
+   @Column
+    private int age;
+    //private Date birthday;
+
+    public User() {}
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+       // this.authToken = authToken;
+        this.age = age;
+        //this.birthday = birthday;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /*public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }*/
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /*public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }*/
+}
---
+++ src/main/java/Practice1/UserFormValidationController.java
@@ -0,0 +1,49 @@
+package practice1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserFormValidationController {
+    @Autowired
+    private UserRepository userRepository;
+
+    //@Override
+    public void addViewController(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(UserForm userForm) {
+        return "home";
+
+    }
+
+    @RequestMapping("/results")
+    public String results(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "results";
+
+    }
+
+    @PostMapping("/")
+    public String checkUserInfo(@Valid UserForm userForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "home";
+        }
+
+        User newUser = new User(userForm.getFirstName(), userForm.getLastName(), userForm.getAge());
+        userRepository.save(newUser);
+        userRepository.findAll();
+        return "redirect:/results";
+    }
+}
